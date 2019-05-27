ee3f888490de6fd1dc01674b9c6dfc72a00e650f
IOT <IOT@gmail.com>
2019-05-27 23:17:18 +0500

    adding files

---
+++ myapple/config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    FB_PAGE_TOKEN: 'EAAMjKrXFr5kBAODXSudWqm1vr1MrL4y6GshZBsajl4pJ7v5NtjjmZBeQbgF4E5LobZAxDSsUNPzAPSyUM4oNlB2gglg4TPWlgixuGtc0mztkuskhieG6jr9cHUgE3SZAqZBffHzZA2gL5ZBlz5okV9zu2yJrze4FUePXCJ1NjVm2QZDZD',
+    FB_VERIFY_TOKEN: 'simple_123',
+    FB_APP_SECRET: '57fad641f39edcacb0f457eed87d016f',
+    SERVER_URL: 'https://tranquil-fjord-25325.herokuapp.com/',
+    GOOGLE_PROJECT_ID: 'test-bot-9ce3b',
+    DF_LANGUAGE_CODE: 'en',
+    GOOGLE_CLIENT_EMAIL: 'dialogflow-chmgjd@test-bot-9ce3b.iam.gserviceaccount.com',
+    GOOGLE_PRIVATE_KEY: '9a9bf388614c476aa2556d46f089907f'
+};
+
+
+
+
+
