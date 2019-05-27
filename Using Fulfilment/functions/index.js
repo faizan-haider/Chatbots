b6dc06125d1aa4624e2b63109a755e5f0b45c4a6
IOT <IOT@gmail.com>
2019-05-28 00:35:27 +0500

    have learned fulfilment more

--- Using Fulfilment/functions/index.js
+++ Using Fulfilment/functions/index.js
@@ -2,9 +2,12 @@ const functions = require('firebase-functions');
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
-//
+
  exports.webhook = functions.https.onRequest((request, response) => {
+
+  console.log("request.body.queryResult.parameters: ", request.body.queryResult.parameters);
+    let params = request.body.queryResult.parameters;
        response.send({
-        fulfillmentText: "Hello world response from webhook"
+        fulfillmentText:`${params.name} Your hotel booking for ${params.persons} have been booked and you will get reply soon at ${params.email}`
        });
  });
